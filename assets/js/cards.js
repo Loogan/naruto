522b9f10f5d0c0bf6c556ede684173479421c942
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2024-11-23 11:21:57 -0300

    Format text content.

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -12,46 +12,157 @@ function getImagePath(imageName, category) {
 }
 
 const CHARS_SHINOBI = [
-  { name: 'Naruto', image: 'naruto.png', description: '' },
-  { name: 'Sasuke', image: 'sasuke.png', description: '' },
-  { name: 'Sakura', image: 'sakura.png', description: '' },
-  { name: 'Kakashi', image: 'kakashi.png', description: '' },
-  { name: 'Minato', image: 'minato.png', description: '' },
-  { name: 'Jiraya', image: 'jiraya.png', description: '' },
-  { name: 'Tsunade', image: 'tsunade.png', description: '' },
-  { name: 'Hashirama', image: 'hashirama.png', description: '' },
-  { name: 'Guy', image: 'guy.png', description: '' },
-  { name: 'Neji', image: 'neji.png', description: '' },
-  { name: 'Hinata', image: 'hinata.png', description: '' },
-  { name: 'Rock Lee', image: 'rock-lee.png', description: '' },
-  { name: 'Ino', image: 'ino.png', description: '' },
-  { name: 'Choji', image: 'choji.png', description: '' },
-  { name: 'Shikamaru', image: 'shikamaru.png', description: '' },
-  { name: 'Shino', image: 'shino.png', description: '' },
-  { name: 'Gaara', image: 'gaara.png', description: '' },
-  { name: 'Sai', image: 'sai.png', description: '' },
-  { name: 'Raikage', image: 'raikage.png', description: '' },
-  { name: 'Rin', image: 'rin.png', description: '' },
-  { name: 'Sarutobi', image: 'sarutobi.png', description: '' },
-  { name: 'Iruka', image: 'iruka.png', description: '' },
-  { name: 'Kurenai', image: 'kurenai.png', description: '' },
+  { name: 'Naruto', 
+    image: 'naruto.png', 
+    description: '' 
+  },
+  { name: 'Sasuke', 
+    image: 'sasuke.png', 
+    description: '' 
+  },
+  { name: 'Sakura', 
+    image: 'sakura.png', 
+    description: '' 
+  },
+  { name: 'Kakashi', 
+    image: 'kakashi.png', 
+    description: '' 
+  },
+  { name: 'Minato', 
+    image: 'minato.png', 
+    description: '' 
+  },
+  { name: 'Jiraya', 
+    image: 'jiraya.png', 
+    description: '' 
+  },
+  { name: 'Tsunade', 
+    image: 'tsunade.png', 
+    description: '' 
+  },
+  { name: 'Hashirama', 
+    image: 'hashirama.png', 
+    description: '' 
+  },
+  { name: 'Guy', 
+    image: 'guy.png', 
+    description: '' 
+  },
+  { name: 'Neji', 
+    image: 'neji.png', 
+    description: '' 
+  },
+  { name: 'Hinata', 
+    image: 'hinata.png', 
+    description: '' 
+  },
+  { name: 'Rock Lee', 
+    image: 'rock-lee.png', 
+    description: '' 
+  },
+  { name: 'Ino', 
+    image: 'ino.png', 
+    description: '' 
+  },
+  { name: 'Choji', 
+    image: 'choji.png', 
+    description: '' 
+  },
+  { name: 'Shikamaru', 
+    image: 'shikamaru.png', 
+    description: '' 
+  },
+  { name: 'Shino', 
+    image: 'shino.png', 
+    description: '' 
+  },
+  { name: 'Gaara', 
+    image: 'gaara.png', 
+    description: '' 
+  },
+  { name: 'Sai', 
+    image: 'sai.png', 
+    description: '' 
+  },
+  { name: 'Raikage', 
+    image: 'raikage.png', 
+    description: '' 
+  },
+  { name: 'Rin', 
+    image: 'rin.png', 
+    description: '' 
+  },
+  { name: 'Sarutobi', 
+    image: 'sarutobi.png', 
+    description: '' 
+  },
+  { name: 'Iruka', 
+    image: 'iruka.png', 
+    description: '' 
+  },
+  { name: 'Kurenai', 
+    image: 'kurenai.png', 
+    description: '' 
+  },
 ];
 
 const CHARS_AKATSUKI = [
-  { name: 'Itachi', image: 'itachi.png', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit' },
-  { name: 'Kisame', image: 'kisame.png', description: '' },
-  { name: 'Pain', image: 'pain.png', description: '' },
-  { name: 'Hidan', image: 'hidan.png', description: '' },
-  { name: 'Deidara', image: 'deidara.png', description: '' },
-  { name: 'Kakusu', image: 'kakusu.png', description: '' },
-  { name: 'Konan', image: 'konan.png', description: '' },
-  { name: 'Orochimaru', image: 'orochimaru.png', description: '' },
-  { name: 'Sasori', image: 'sasori.png', description: '' },
-  { name: 'Zetsu', image: 'zetsu.png', description: '' },
-  { name: 'Obito', image: 'obito.png', description: '' },
-  { name: 'Madara', image: 'madara.png', description: '' },
-  { name: 'Kabuto', image: 'kabuto.png', description: '' },
-  { name: 'Zabuza', image: 'zabuza.png', description: '' },
+  { name: 'Itachi', 
+    image: 'itachi.png', 
+    description: 'lorem ipsum dolor sit amet consectetur adipiscing elit' 
+  },
+  { name: 'Kisame', 
+    image: 'kisame.png', 
+    description: '' 
+  },
+  { name: 'Pain', 
+    image: 'pain.png', 
+    description: '' 
+  },
+  { name: 'Hidan', 
+    image: 'hidan.png', 
+    description: '' 
+  },
+  { name: 'Deidara', 
+    image: 'deidara.png', 
+    description: '' 
+  },
+  { name: 'Kakusu', 
+    image: 'kakusu.png', 
+    description: '' 
+  },
+  { name: 'Konan', 
+    image: 'konan.png', 
+    description: '' 
+  },
+  { name: 'Orochimaru', 
+    image: 'orochimaru.png', 
+    description: '' 
+  },
+  { name: 'Sasori', 
+    image: 'sasori.png', 
+    description: '' 
+  },
+  { name: 'Zetsu', 
+    image: 'zetsu.png', 
+    description: '' 
+  },
+  { name: 'Obito', 
+    image: 'obito.png', 
+    description: '' 
+  },
+  { name: 'Madara', 
+    image: 'madara.png', 
+    description: '' 
+  },
+  { name: 'Kabuto', 
+    image: 'kabuto.png', 
+    description: '' 
+  },
+  { name: 'Zabuza', 
+    image: 'zabuza.png', 
+    description: '' 
+  },
 ];
 
 const shinobiSmallSwiperWrapper = document.querySelector('#carrouselSwiper .swiper-wrapper');
